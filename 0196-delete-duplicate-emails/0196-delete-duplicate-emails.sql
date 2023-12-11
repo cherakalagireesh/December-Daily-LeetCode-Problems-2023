ca88f2be9d742ca8addad1f79fd0021f5303f4fc
Cherakala Gireesh <135722119+cherakalagireesh@users.noreply.github.com>
2023-12-11 17:46:17 +0530

    Time: 2457 ms (5.07%), Space: 0B (100.00%) - LeetHub

---
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -0,0 +1,7 @@
+/* Write your PL/SQL query statement below */
+
+DELETE FROM PERSON
+WHERE ID NOT IN( SELECT MIN(ID)
+                    FROM PERSON
+                    GROUP BY EMAIL   
+                   );
