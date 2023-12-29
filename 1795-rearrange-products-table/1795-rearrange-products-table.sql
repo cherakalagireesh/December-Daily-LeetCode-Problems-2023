a04359e153fb6842ddbd8ec1a07fec776b194cf8
Cherakala Gireesh <135722119+cherakalagireesh@users.noreply.github.com>
2023-12-29 17:19:06 +0530

    Time: 1645 ms (23.53%), Space: 0B (100.00%) - LeetHub

---
+++ 1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -0,0 +1,14 @@
+/* Write your PL/SQL query statement below */
+
+
+SELECT product_id, 'store1' store, store1 AS price
+FROM Products
+WHERE store1 IS NOT NULL
+UNION 
+SELECT product_id, 'store2' store, store2 AS price
+FROM Products
+WHERE store2 IS NOT NULL
+UNION 
+SELECT product_id, 'store3' store, store3 AS price
+FROM Products
+WHERE store3 IS NOT NULL;
