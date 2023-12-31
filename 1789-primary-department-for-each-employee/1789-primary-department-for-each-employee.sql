b15e72845d1f5f9153e192c050f4535e1514c9b4
Cherakala Gireesh <135722119+cherakalagireesh@users.noreply.github.com>
2023-12-31 20:22:54 +0530

    Time: 1246 ms (54.71%), Space: 0B (100.00%) - LeetHub

---
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -0,0 +1,7 @@
+/* Write your PL/SQL query statement below */
+
+select employee_id , department_id from employee
+where 
+employee_id in (select employee_id from employee 
+group by employee_id having count(employee_id) = 1)
+or primary_flag = 'Y'
